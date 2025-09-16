02fe16b3fb7a92137b66f2899cca195fa81091d4
marcwalkr <47399441+marcwalkr@users.noreply.github.com>
2025-09-16 13:44:49 -0500

    Add addBookToLibrary() function

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const myLibrary = [];
+
 function Book(id, title, author, category, genre, pages, yearPublished, read) {
   this.id = id;
   this.title = title;
@@ -7,4 +9,10 @@ function Book(id, title, author, category, genre, pages, yearPublished, read) {
   this.pages = pages;
   this.yearPublished = yearPublished;
   this.read = read;
+}
+
+function addBookToLibrary(title, author, category, genre, pages, yearPublished, read) {
+  const uuid = crypto.randomUUID();
+  const book = new Book(uuid, title, author, category, genre, pages, yearPublished, read);
+  myLibrary.push(book);
 }

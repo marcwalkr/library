52ef84313a32575a18902450abba2c07280aa30e
marcwalkr <47399441+marcwalkr@users.noreply.github.com>
2025-09-16 13:04:08 -0500

    Add Book constructor function

---
+++ script.js
@@ -0,0 +1,10 @@
+function Book(id, title, author, category, genre, pages, yearPublished, read) {
+  this.id = id;
+  this.title = title;
+  this.author = author;
+  this.category = category;
+  this.genre = genre;
+  this.pages = pages;
+  this.yearPublished = yearPublished;
+  this.read = read;
+}
